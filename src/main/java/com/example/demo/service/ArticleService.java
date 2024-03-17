f0169e582c32de945e34ac7b5d77600eabe62faa
Lysepten <tlstlsrbrb11@gmail.com>
2024-03-17 21:43:31 +0900

    Spring 프레임워크를 활용하여 MVC 패턴 기반 게시판 구현

---
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -0,0 +1,66 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.ArticleRepository;
+import com.example.demo.util.Ut;
+import com.example.demo.vo.Article;
+import com.example.demo.vo.ResultData;
+
+@Service
+public class ArticleService {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+	}
+
+	public Article getForPrintArticle(int id) {
+		Article article = articleRepository.getForPrintArticle(id);
+
+		return article;
+	}
+
+
+	public ResultData userCanDelete(int loginedMemberId, Article article) {
+
+
+		return ResultData.from("S-1", Ut.f("%d번 글이 삭제 되었습니다", article.getId()));
+	}
+
+	public ResultData userCanModify(int loginedMemberId, Article article) {
+
+
+		return ResultData.from("S-1", Ut.f("%d번 글을 수정했습니다", article.getId()));
+	}
+
+	public ResultData<Integer> writeArticle(String title, String body) {
+		articleRepository.writeArticle(title, body);
+
+		int id = articleRepository.getLastInsertId();
+
+		return ResultData.from("S-1", Ut.f("%d번 글이 생성되었습니다", id), "id", id);
+	}
+
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
+	}
+
+}
